5eeeffdc8d2df576848f385972f5a8156904066b
ChristopherSparling <christopher.j.sparling@gmail.com>
2018-10-14 14:43:46 -0400

    Checking out TravisCI

--- tests/app-test.py
+++ tests/app-test.py
@@ -9,6 +9,6 @@ class MyTest(unittest.TestCase):
         self.assertEqual(my_function(-1, -1), -2)
         self.assertEqual(my_function(1.0, 1), 2)        
         self.assertEqual(my_function(1.1, 1.1), 2.2) 
-
+        self.assertEqual(my_function(1,1),3)
 if __name__ == '__main__':
     unittest.main()  
